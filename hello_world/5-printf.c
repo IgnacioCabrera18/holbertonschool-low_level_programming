207259b2ade3201582ec100fe90ff32353352fc6
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-18 16:47:29 +0000

    5. Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - es funcion principal
+ *Return: devuelve a cero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
