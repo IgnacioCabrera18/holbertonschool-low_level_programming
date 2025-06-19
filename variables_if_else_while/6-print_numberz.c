e7fd320b7390c2fef8b39677e7fcd8ab3cc72378
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-19 14:44:13 +0000

    6. Numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int numbers;
 
-	for (numbers = 0; numbers < 10; numbers++)
-	printf("%d", numbers);
+	for (numbers = '0'; numbers <= '9'; numbers++)
+	putchar(numbers);
 
-	printf("\n");
+	putchar('\n');
 	return (0);
 }
