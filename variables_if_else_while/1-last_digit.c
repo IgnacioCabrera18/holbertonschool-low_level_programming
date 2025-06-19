84a7d573dd172a8e1ca5c94b54e8b3ff77055713
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-19 12:16:03 +0000

    1. The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,16 +10,25 @@
  */
 int main(void)
 {
+	/*se agrerga el ultimoN como numero*/
 	int n, ultimoN;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	/**
+	 * el %diez inidica que sera el ultimo digito
+	 * en caso de ser %cien serian los ultimos dos digitos
+	 */
 	ultimoN = n % 10;
-	if (n > 5)
+	if (ultimoN > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, ultimoN);
-	else if (n == 0)
+	else if (ultimoN == 0)
 		printf("Last digit of %d is %d and is 0\n", n, ultimoN);
-	else if (n < 6)
+	/**
+	 * el && significa and,sirve para asignarle dos resultados a ultimoN
+	 * el ! significa diferente a, como decir no es igual =
+	 */
+	else if (ultimoN < 6 && ultimoN != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ultimoN);
 
 	return (0);
