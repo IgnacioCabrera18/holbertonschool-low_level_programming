12debb0c03758a389187f62640458e7dfd8f96df
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-19 12:40:13 +0000

    2. alphabet

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,6 +16,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+		/*el %d remplaza el valor de la variable y imprime el resultante*/
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
