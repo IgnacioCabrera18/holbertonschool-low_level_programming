74bd7d89cf9ecdf73cb1d3b496bdcacfc5fdfcd5
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-20 15:00:04 +0000

    0. _putchar

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,19 +1,7 @@
 /**
-*File:main.h
-*Author:Pericles Adjovi
-*Desc:header file containing functions declaration
-*/
-
-void print_alphabet(void);
-void jack_bauer(void);
-void print_alphabet_x10(void);
-void times_table();
-void print_to_98(int);
-void print_times_table(int);
-int _putchar(char);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int add(int n, int m);
-int print_last_digit(int n);
+ *main.h es un encabezado que yo cree no es una ya creada
+ *por lo tanto no va a ir entre <> sino entre "" despues del #iclude
+ *aca tengo que incluir todos los prototypes que me especifique el ejercicio
+ *de lo contrario saltara error
+ */
+int _putchar(char c);
