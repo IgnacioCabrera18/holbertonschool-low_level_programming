24d1ce6f7e0753bd47342cfa6296f7f8e4156864
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-20 18:50:36 +0000

    5. Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *print_sign - is the main function
+ *@n: Returns 1 and prints + if n is greater than zero
+ *Return: Returns 1 and prints + if n is greater than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	return (1);
+	}
+	else if (n == 0)
+	{
+	return (0);
+	}
+	else
+	{
+	return (-1);
+	}
+}
