afd4fd5181fa96ddf7a7bb48e3e02133f803f7af
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-20 16:36:39 +0000

    1. alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,18 +1,10 @@
 #include "main.h"
 
 /**
- *main - is the main function
- *Return: returns to zero
- */
-
-/**
- *no se escribe main porque este ejercico es escribir una funcion
- *se arega main cuando es un programa y en este caso no lo es
- *no es necesario agregar return 0
- *ya que el primer void sinifica sin retorno
- *y el ultimo que no devuelve nada
- */
-
+  * print_alphabet - Make the alphabet
+  *
+  * Return: void
+  */
 void print_alphabet(void)
 {
 	char alphabet;
