96411d4dc1192b1c300ed4ead39b3517be6b8632
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-24 17:13:34 +0000

    11. 98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - is the main function
+ *@n: is an int
+ *Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+		for (; n <= 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	else if (n >= 98)
+		for (; n >= 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	printf("\n");
+}
