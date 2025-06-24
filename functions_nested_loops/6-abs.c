a93d85bbf4be5626b64b2dc77d2f2f7a9c2e1b06
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-24 13:09:15 +0000

    6. There is no such thing as absolute value in this world.

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -11,6 +11,11 @@ int _abs(int r)
 {
 	if (r < 0)
 	{
+	/**
+	 * escribir -r significa que
+	 * se devuelve lo contrario a r, es decir
+	 * si es negativo devuelve positivo
+	 */
 	return (-r);
 	}
 	else
