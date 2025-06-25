0016d55750cbe9f940c904992e490e4e0a9e82cb
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-25 17:20:33 +0000

    1. isdigit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isdigit - is the main function
+ *@c: is an int
+ *Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+	if (c <= '0' && c >= '0')
+		return (1);
+	else
+		return (0);
+}
