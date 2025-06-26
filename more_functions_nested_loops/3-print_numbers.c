50ceaf145ac7ed260cd657804549e6e23f503865
root <ignaciocabrera1832006@gmail.com>
2025-06-26 16:48:43 +0000

    3. The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - is the main function
+ *Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+
+	_putchar ('\n');
+}
