b38a984ea8b22b364007661494fa8d4b0513d847
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-27 14:30:58 +0000

    9. Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - is the main function
+ *Return: nothing
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		/**
+		 * % 15 si es multiplo de 3 y 5
+		 * se podria poner tambien:
+		 * i % 3 == 0 && i % 5 == 0
+		 */
+		if (i % 15 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
