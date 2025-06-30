95a3d87bf62c9948ed7ccb9a9f706cb73d5b3762
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-06-30 16:48:44 +0000

    6. par

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
-void
-puts2(char *str)
-{
-int i;
+/**
+ *puts2 - is the main function
+ *@str: is sequence of characters
+ */
 
-for (i = 0; str[i] != '\0'; i++)
-if (i % 2 == 0)
-_putchar(str[i]);
-_putchar('\n');
-}	
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	_putchar('\n');
+}
