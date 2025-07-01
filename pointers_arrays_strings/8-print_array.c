bd2ff18d866ab4997de4db28fa8146b96ec7d1ab
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-01 11:57:37 +0000

    8. Arrays are not pointers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,24 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *
- *
+ *print_array - is the main function
+ *@a: is an aray
+ *@n: is [n]
  */
 
 void print_array(int *a, int n)
 {
+	for (n = 0; n <= 4; n++)
+	{
+		if (n < 4)
+		{
+			printf("%d, ", a[n]);
+		}
+		else if (n == 4)
+		{
+			printf("%d\n", a[n]);
+		}
 
+	}
+}
