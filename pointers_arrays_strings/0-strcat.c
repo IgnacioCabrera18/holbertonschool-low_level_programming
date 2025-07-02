be561bad352a632454aa880879695f3d2f48da30
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-02 17:22:41 +0000

    100. atoi

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *
+ *
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	dest[i];
+	src[i];
+	
+	for (i = 0; src[i] != '\0'; i++)
+		strcat;
+	return(dest);
+}
