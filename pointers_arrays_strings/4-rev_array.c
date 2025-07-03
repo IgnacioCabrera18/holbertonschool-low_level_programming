4cdc01ba86e63bc66456726660367ed0d2dfbc8e
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-03 19:22:12 +0000

    4. I am a kind of paranoid in reverse.

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -10,10 +10,10 @@ void reverse_array(int *a, int n)
 {
 	int i, j;
 
-	for (j = 0; j < (n / 2); j++)
+	for (i = 0; i < ((n - 1) / 2); i++)
 	{
-		i = a[j];
-		a[j] = a[n - 1 - i];
+		j = a[i];
+		a[i] = a[n - 1 - i];
 		a[n - 1 - i] = j;
 	}
 }
