619c0a8517db2a0861f51fdb90914df142195404
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-04 16:53:26 +0000

    6. Expect the best. Prepare for the worst.

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ *
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char array[] = " \t\n,;.!?\"(){}";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/*si la primera letra fuera minusucula la cambia a mayuscula*/
+		if (s[0] >= 'a' && s[0] <= 'z')
+			s[0] -= 32;
+		if (s[i - 1] >= 'a' && s[i - 1] <= 'z')
+			/*continue skipea lo siguiente si se cumple if*/
+			continue;
+		/*recorre la array*/
+		for (j = 0; array[j] != '\0'; j++)
+		{
+			/**
+			 *si antes de la letra hay un separador y esa letra es minuscula
+			 *la cambia a mayuscula
+			 */
+			if (s[i - 1] == array[j] && s[i] <= 'z' && s[i] >= 'a')
+			{
+				s[i] = s[i] - 32;
+			}
+		}
+	}
+	return (s);
+}
