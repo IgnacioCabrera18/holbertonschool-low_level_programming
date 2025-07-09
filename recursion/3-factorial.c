5ece77eb7220ec7ae08c8158c39fa42e87a04553
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-09 16:43:16 +0000

    3. factorial

---
+++ recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *factorial - is the main function
+ *@n: is an int of factorial
+ *Return: factorial
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
