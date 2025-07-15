c1baf5dffd4d3822eb25dd8a17182ae5d3a31639
IgnacioCabrera18 <ignaciocabrera18>
2025-07-15 15:59:34 +0000

    no hay !!!

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *string_nconcat - conectar dos strings
+ *@s1: primer str
+ *@s2: segundo str
+ *@n: n bytes de memoria
+ *Return: NULL o a
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *a;
+	unsigned int i, j, len1 = 0, len2 = 0;
+
+	if (s1 != NULL)
+	{
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+		{
+			continue;
+		}
+	}
+	if (s2 != NULL)
+	{
+		/*corta antes de n bytes de memoria*/
+		/*por eso no imrpime toda la str*/
+		for (len2 = 0; s2[len2] != '\0' && len2 < n; len2++)
+		{
+			continue;
+		}
+	}
+	a = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < len1; i++)
+	{
+		a[i] = s1[i];
+	}
+	for (j = 0; j < len2; j++)
+	{
+		a[i + j] = s2[j];
+	}
+	return (a);
+}
