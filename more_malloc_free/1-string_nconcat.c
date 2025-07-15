b9068e830012afa9426bf92bb993f45bd9a70255
IgnacioCabrera18 <ignaciocabrera18>
2025-07-15 17:39:50 +0000

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,12 +30,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (a == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
-	{
 		a[i] = s1[i];
-	}
+	/*corta cuando sea NULL o sea menor a n*/
 	for (j = 0; s2[j] != '\0' && j < n; j++)
-	{
 		a[i + j] = s2[j];
-	}
 	return (a);
 }
