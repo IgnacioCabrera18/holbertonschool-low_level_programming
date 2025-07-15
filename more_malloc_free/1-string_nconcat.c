15c40ceac06327c605a5b5cac26621f16ed97d25
IgnacioCabrera18 <ignaciocabrera18>
2025-07-15 17:31:42 +0000

    task1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  *string_nconcat - conectar dos strings
@@ -9,37 +10,30 @@
  *Return: NULL o a
  */
 
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *a;
-	unsigned int i, j, len1 = 0, len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
+	a = NULL;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	for (len1 = 0; s1[len1] != '\0'; len1++)
-	{
-		;
-	}
-	for (len2 = 0; (s2[len2] != '\0' && len2 < n); len2++)
-	{
-		;
-	}
+		continue;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		continue;
 	a = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (a == NULL)
-	{
 		return (NULL);
-	}
 	for (i = 0; i < len1; i++)
 	{
 		a[i] = s1[i];
 	}
-	for (j = 0; j < len2; j++)
+	for (j = 0; s2[j] != '\0' && j < n; j++)
 	{
 		a[i + j] = s2[j];
 	}
