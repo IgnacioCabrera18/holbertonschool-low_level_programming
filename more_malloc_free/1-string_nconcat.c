32218073332bc44982d29a51f4370f185ce9e58a
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-16 12:14:05 +0000

    gracias cape

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -34,5 +34,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	/*corta cuando sea NULL o sea menor a n*/
 	for (j = 0; s2[j] != '\0' && j < n; j++)
 		a[i + j] = s2[j];
+	a[i + j] = '\0';
 	return (a);
 }
