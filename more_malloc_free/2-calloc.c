0a9ea590bc61bd1f854a20658f1852e7faa74ad2
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-17 17:51:32 +0000

    2. calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ *_calloc - se utiliza para alocar memoria dinámica y además inicializarla 0
+ *@nmemb: El número de elementos que sequiere almacenar enelbloque dememoria
+ *@size: El tamaño en bytes de cada elemento del arregl
+ *Return: NULL o a
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
@@ -22,6 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
+	/*establece la memoria asignada en 0*/
 	for (i = 0; i < (nmemb * size); i++)
 	{
 		a[i] = 0;
