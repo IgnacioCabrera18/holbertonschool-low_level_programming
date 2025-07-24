6926138b16b5794f0293495082a8caf8face748c
root <IgnacioCabrera18>
2025-07-24 11:30:58 +0000

    1.list_len

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,15 +1,23 @@
 #include "lists.h"
 
 /**
- *
- *
- *
+ *dlistint_len - retornar el largo de la lista
+ *@h: puntero al encabezado de la lista
+ *Return: elements
  */
 
 size_t dlistint_len(const dlistint_t *h)
 {
 	int elements = 0;
-
+	
+	if (h == NULL)
+	{
+		return (elements);
+	}
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
 	while (h != NULL)
 	{
 		elements++;
