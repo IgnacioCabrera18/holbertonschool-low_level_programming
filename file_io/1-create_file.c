7558da9a922e2e29cf4913ab0115a1fa5eb702d3
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-27 01:18:15 +0000

    1.create_file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -11,7 +11,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int files, len = strlen(text_content);
+	int files;
 	ssize_t bytes_write;
 	mode_t permissions = S_IRUSR | S_IWUSR;
 
@@ -23,8 +23,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		bytes_write = write(files, text_content, len);
-		if (bytes_write == -1 || bytes_write != len)
+		bytes_write = write(files, text_content, strlen(text_content));
+		if (bytes_write == -1 || (size_t)bytes_write != strlen(text_content))
 		{
 			close(files);
 			return (-1);
