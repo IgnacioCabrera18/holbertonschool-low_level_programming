f424c37e94ff21a69c2446c5c9fc588cf22f3855
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-28 16:47:31 +0000

    0.ht

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -12,10 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *ht;
 	unsigned long int i;
 
+	if (size <= 0)
+		return (NULL);
+
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->array = calloc(size, sizeof(hash_table_t));
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_table_t *) * size);
 	if (ht->array == NULL)
 	{
 		free(ht);
