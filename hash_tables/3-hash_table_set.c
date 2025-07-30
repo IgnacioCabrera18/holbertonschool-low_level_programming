34d4f3d449bdd9941a94201dd7c8d0e8c1ade5da
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-07-30 12:05:49 +0000

    3.adds an element to the hash table.

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,54 @@
+#include "hash_tables.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new, *temp;
+	unsigned long int index;
+	char *new_value;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(temp->value);
+			temp->value = new_value;
+			return (1);
+		}
+		temp = temp->next;
+	}
+	new =malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
